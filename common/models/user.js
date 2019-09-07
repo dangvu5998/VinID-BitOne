817fff664cd7f0390e47c974ef8e0a762083938a
hoangph7298 <hoangph7298@gmail.com>
2019-09-08 00:20:33 +0700

    update api user

--- common/models/user.js
+++ common/models/user.js
@@ -1,6 +1,30 @@
 'use strict';
+let to = require('await-to-js').to;
+// let vinJson  = require('../utils/vin-app');
 
 module.exports = function(User) {
 
+    User.createUser = async function(req) {
+        let userData = {
+            userId:req.userId,
+            fullName:req.fullName,
+            gender:req.gender,
+            age:req.age
+        }
+        User.upsert(userData)
+        let context = vinJson.createJson();
+        return [200, 'success']
+    }
 
+
+    User.remoteMethod(
+        'createUser', {
+            http: {path: '/', verb: 'post'},
+            accepts: [
+                {arg: 'req', type: 'object', 'http': {source: 'req'}}            ],
+            returns: [
+                {arg: 'status', type: 'number'},
+                {arg: 'message', type: 'string'}],
+        },
+    )
 };
