1d42ee7e355f80108ce0d2a27d9213089e22434b
TriVu <dangvu5998@gmail.com>
2019-09-07 18:56:54 +0700

    fix general form

--- common/utils/vin-app.js
+++ common/utils/vin-app.js
@@ -1,13 +1,12 @@
-function appFormTemplate(submitButton, resetButton, elements) {
+function appFormTemplate(additionAttr) {
   let result = {
     data: {
       metadata:{
         app_name: "Sân chơi trí tuệ",
-        app_id: 26121998,
-        title: "Tiêu đề trong card",
-        submit_button: submitButton,
-        reset_button: resetButton,
-        elements: elements
+        app_id: 123456,
+        ...additionAttr
       }
     }
   }
+  return result;
+}
