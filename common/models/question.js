802f606534cf6cec20a0148f1f2d7a1bada72d5f
ngocanh2162 <ngocanh2162@gmail.com>
2019-09-07 21:41:17 +0700

    add utils

--- common/models/question.js
+++ common/models/question.js
@@ -1,6 +1,35 @@
 'use strict';
 
+let vjson = require('../utils/vinid-json')
+let app = require('../../server/server')
+let to = require('await-to-js').to
+
 module.exports = function(Question) {
+    Question.playerGetQuestion = async function(userContestId){
+        let questionForm = vjson.createJson()
+        let Question = app.models.Question
+        let Contest = app.models.Contest
+        let UserContest = app.models.UserContest
+        let [err0, userContest] = await to(UserContest.find({where: {userContestId: userContestId}}))
+        if (err0){
+
+        }
+        let [err1, contest] = await to(Contest.find({where: {contestId: userContest.contestId}}))
+        let [err2, question] = await to(Question.find({where: {questionId: contest.questions[userContest.currentQuestion]}}))
+        if (err2) {
+            console.log(err)
+            vjson.addElement(orderForm, {
+                type: 'text',
+                style: 'paragraph',
+                content: 'Error when finding current question'
+            })
+            console.log(err)
+            return questionForm;
+        }
+        let element = vjson.questionToForm(question)
+        vjson.addElements(questionForm, element)
+        return questionForm
+    }
 
   Question.createQuestion = async (req, contestId, description, answers) => {
     console.log(req.userId);
@@ -19,4 +48,14 @@ module.exports = function(Question) {
       {arg: 'data', type: 'object'},
     ]
   })
+    Question.remoteMethod(
+        'playerGetQuestion', {
+            path: '/playerGetQuestion',
+            accepts: [
+                {arg: 'userContestId', type: 'string', required: 'true'},
+                {arg: 'currentQuestion', type: 'number', required: 'true'}
+            ],
+            returns: {arg: 'data', type: 'object'}
+        }
+    )
 };
--- common/utils/vin-app.js
+++ common/utils/vin-app.js
@@ -10,3 +10,62 @@ function appFormTemplate(additionAttr) {
   }
   return result;
 }
+
+
+
+module.exports = {
+  createJson: function() {
+      return {
+          "metadata": {
+              "app_name": "bitZero",
+              "title": "bitZero - Tự định nghĩa cách bạn mua hàng",
+              "submit_button": {
+                  "label": "Gửi thông tin",
+                  "background_color": "#6666ff",
+                  "cta": "request",
+                  "url": "https://teambit.tech/api/send-response"
+              },
+              "reset_button": {
+                  "label": "Xoá bản ghi",
+                  "background_color": "#669999"
+              },
+              "elements": []
+          }
+      }
+  },
+  
+  addElement: function(json, element) {
+      json.metadata.elements.push(element);
+  },
+
+  addElements: function(json, elements) {
+      for (i in elements) {
+          element = elements[i]
+          this.addElement(json, element);
+      }
+  },
+
+  actionToElement: function(action) {
+      var element = {}
+      if (action.valueType == "input"){
+          element.type = "input"
+          element.inputType = "number"
+          element.label = action.actionName
+          element.required = true
+          element.name = action.actionName
+      }
+      else if (action.valueType == "radio"){
+          element.type = "radio"
+          element.display = "inline"
+          element.label = action.infomation
+          element.name = action.actionName
+      }
+      else if (action.valueType == "checkbox"){
+          element.type = "checkBox"
+          element.display = "inline"
+          element.label = action.infomation
+          element.name = action.actionName
+      }
+      return element
+  }
+}
