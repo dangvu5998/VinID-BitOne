aaa157d15f1440c27c6e427b60f67119791cc3ec
TriVu <dangvu5998@gmail.com>
2019-09-07 18:49:41 +0700

    add general form

---
+++ common/utils/vin-app.js
@@ -0,0 +1,13 @@
+function appFormTemplate(submitButton, resetButton, elements) {
+  let result = {
+    data: {
+      metadata:{
+        app_name: "Sân chơi trí tuệ",
+        app_id: 26121998,
+        title: "Tiêu đề trong card",
+        submit_button: submitButton,
+        reset_button: resetButton,
+        elements: elements
+      }
+    }
+  }
