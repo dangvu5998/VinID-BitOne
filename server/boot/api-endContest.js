e0c2456b1c90e1a02fab7afb9000a90f2fffa84b
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-09-07 18:40:15 +0700

    KK

--- server/boot/api-endContest.js
+++ server/boot/api-endContest.js
@@ -6,12 +6,23 @@ module.exports = function (app) {
                     "app_name": "Contest",
                     "app_id": 123456,
                     "title": "Kết thúc cuộc thi",
+                    "text_view": {
+                        "type": "text",
+                        "style": "heading",
+                        "content": "Chúc mừng bạn đã hoàn thành contest, ấn OK để xem ranking"
+                    },
                     "ok_button": {
                         "label": "Ok",
                         "background_color": "#6666ff", //Background color of submit button
                         "cta": "request",
                         "url": "http://bitone.herokuapp.com/api-endContest"
-                    }
+                    },
+                    "elements": [
+                        {
+                            "type": "input",
+
+                        }
+                    ]
                 }
             }
         });
