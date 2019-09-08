3b24d350a1acdd9f9a8b71563b7ab1a378f42e46
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-09-08 15:05:40 +0700

    Update user.js

--- common/models/user.js
+++ common/models/user.js
@@ -32,6 +32,9 @@ module.exports = function(User) {
                 "style": "heading",
                 "content": "Are you ready?"
             },
+            {
+              "type": "input"
+            }
           ]
         }
       };
