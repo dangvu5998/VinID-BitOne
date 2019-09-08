ca6d2b2e236986cad4c2ac0cba913376635bc637
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-09-08 14:39:35 +0700

    dd

---
+++ server/boot/api-addUser.js
@@ -0,0 +1,37 @@
+module.exports = function (app) {
+    app.get('/api-addUser', function (req, res) {
+        return res.json({
+            "data": {
+                "metadata": {
+                    "app_name": "Contest",
+                    "app_id": 123456,
+                    "title": "Nhập thông tin",
+                    "submit_button": {
+                        "label": "Ok",
+                        "background_color": "#6666ff", //Background color of submit button
+                        "cta": "request",
+                        "url": ""
+                    },
+                    "elements": [
+                        {                            
+                            "type": "input",
+                            "name": "userName",
+                            "input_type": "text",
+                            "label": "User Name",
+                            "required": true,
+                            "placeholder": "User Name"
+                        },
+                        {
+                            "type": "input",
+                            "input_type": "text",
+                            "name": "userAge",
+                            "label": "User Age",
+                            "required": false,
+                            "placeholder": "Contest Description"
+                        },
+                    ]
+                }
+            }
+        });
+    });
+}
