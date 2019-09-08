03528d356973b4d5482f14597a1750c16d18a64f
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-09-08 14:24:05 +0700

    Update contest.js

--- common/models/contest.js
+++ common/models/contest.js
@@ -3,8 +3,8 @@ let to = require('await-to-js').to
 var app = require('../../server/server');
 //var Question = app.models.Question;
 
-module.exports = function(Contest) {
-	Contest.createContest = async (req, res, contestName, contestDes, Question, Score, TimeStart, TimeOut  ) => {
+module.exports = function (Contest) {
+	Contest.createContest = async (req, res, contestName, contestDes, Question, Score, TimeStart, TimeOut) => {
 		console.log(req.userId)
 		qs = Question.split(';');
 		sc = Score.split(';');
@@ -19,261 +19,265 @@ module.exports = function(Contest) {
 
 		Contest.upsert(ContestData)
 		return res.json({
-            "data": {
-                "metadata": {
-                    "app_name": "Contest",
-                    "app_id": 123456,
-                    "title": "BIT",
-                    "submit_button": {
-                    	"label": "Save",
-                    	"background_color": "#6666ff", 
-                        "cta": "request",
-                        "url": ""
-                    },
-                     "reset_button": {
-                    	"label": "Reset",
-                    	"background_color": "#6666ff", 
-                        "cta": "request",
-                        "url": "http://bitone.herokuapp.com//createContest"
-                    },
-                    "elements": [
-                    {
-                        "type": "text",
-                        "style": "heading",
-                        "content": "Ban da tao contest thanh cong!"
-                    }
-                    ]
-                }
-            }
-        }); 
-    }
-    
-    Contest.getDescription = async (req) =>{
-        let contestId = 1
-        let [err, contest] = await to(Contest.findOne({where: {contestId: contestId/*req.header.contestId*/}}));
-        console.log(req.userId);
-        console.log(req)
-        return {
-            "metadata": {
-              "app_name": "Contest",
-              "app_id": 123456,
-              "title": "BIT Contest",
-              "submit_button": {
-                "label": "Go to contest",
-                "background_color": "#6666ff", 
-                "cta": "request",
-                "url": "http://bitzero.herokuapp.com/api/Users/"+req.body.userId+"/"+contestId.toString()
-              },
-              "elements": [
-                {
-                    "type": "text",
-                    "style": "heading",
-                    "content": contest.contestDes
-                },
-              ]
-            }
-        };
-    }
+			"data": {
+				"metadata": {
+					"app_name": "Contest",
+					"app_id": 123456,
+					"title": "BIT",
+					"submit_button": {
+						"label": "Save",
+						"background_color": "#6666ff",
+						"cta": "request",
+						"url": ""
+					},
+					"reset_button": {
+						"label": "Reset",
+						"background_color": "#6666ff",
+						"cta": "request",
+						"url": "http://bitone.herokuapp.com//createContest"
+					},
+					"elements": [
+						{
+							"type": "text",
+							"style": "heading",
+							"content": "Ban da tao contest thanh cong!"
+						}
+					]
+				}
+			}
+		});
+	}
 
-    Contest.getNextQuestion = async(req,contestId,numberQuestion,trueAnswer,score,indexQuestion)=>{
+	Contest.getDescription = async (req) => {
+		let contestId = 1
+		let userId = req.body.userId
+		let [err, contest] = await to(Contest.findOne({ where: { contestId: contestId/*req.header.contestId*/ } }));
+		console.log(req.userId);
+		console.log(req)
+		return {
+			"metadata": {
+				"app_name": "Contest",
+				"app_id": 123456,
+				"title": "BIT Contest",
+				"submit_button": {
+					"label": "Go to contest",
+					"background_color": "#6666ff",
+					"cta": "request",
+					"url": ""
+				},
+				"elements": [
+					{
+						"type": "text",
+						"style": "heading",
+						"content": contest.contestDes
+					},
+					{
+						"type": "input",
+					},
+				]
+			}
+		};
+	}
 
-      let lastAnswer = req.body.Answers[0];
+	Contest.getNextQuestion = async (req, contestId, numberQuestion, trueAnswer, score, indexQuestion) => {
 
-      if (lastAnswer===trueAnswer) score = (Number(score)+1).toString();
+		let lastAnswer = req.body.Answers[0];
 
-      if (Number(indexQuestion)>Number(numberQuestion)-1){
-        return {
-          "metadata": {
-            "app_name": "Contest",
-            "app_id": 123456,
-            "title": "Kết thúc cuộc thi",
-            "submit_button": {
-                "label": "Ok",
-                "background_color": "#6666ff", //Background color of submit button
-                "cta": "request",
-                "url": "http://bitone.herokuapp.com/api-ranking"
-            },
-            "elements": [
-                {
-                    "type": "text",
-                    "style": "paragraph",
-                    "content": "Chúc mừng bạn đã hoàn thành contest! \n "
-                            + "Điểm của bạn là " +score+"\n"+"Ấn OK để xem ranking"
-                },
-                {
-                    "type": "input",
-                }
-            ]
-          }
-        }
-      }
+		if (lastAnswer === trueAnswer) score = (Number(score) + 1).toString();
 
-      let [err, contest] = await to(Contest.findOne({where: {contestId: Number(contestId)}}));
-      
-      var Question = app.models.Question;
-      let contentQuestion  = await Question.findOne({where:{questionId:contest.question[Number(indexQuestion)]}});
+		if (Number(indexQuestion) > Number(numberQuestion) - 1) {
+			return {
+				"metadata": {
+					"app_name": "Contest",
+					"app_id": 123456,
+					"title": "Kết thúc cuộc thi",
+					"submit_button": {
+						"label": "Ok",
+						"background_color": "#6666ff", //Background color of submit button
+						"cta": "request",
+						"url": "http://bitone.herokuapp.com/api-ranking"
+					},
+					"elements": [
+						{
+							"type": "text",
+							"style": "paragraph",
+							"content": "Chúc mừng bạn đã hoàn thành contest! \n "
+								+ "Điểm của bạn là " + score + "\n" + "Ấn OK để xem ranking"
+						},
+						{
+							"type": "input",
+						}
+					]
+				}
+			}
+		}
 
-      return {
-        "metadata": {
-          "app_name": "Contest",
-          "app_id": 123456,
-          "title": "BIT",
-          "submit_button": {
-            "label": "Next questions",
-            "background_color": "#6666ff", 
-            "cta": "request",
-            "url": "http://bitzero.herokuapp.com/api/Contests/"+contestId+"/number/"+numberQuestion+"/right/"+contentQuestion.trueAnswer+"/score/"+score+"/nextQuestion/"+(Number(indexQuestion)+1).toString()
-          },
-          "elements": [
-            {
-                "type": "text",
-                "style": "heading",
-                "content": contentQuestion.content
-            },
-            {
-              "type": "SCORE",
-              "style": "heading",
-              "content": score
-            },
-            {
-                "label": "Câu trả lời",
-                "type": "radio/checkbox",
-                "display_type": "inline/dialog", 
-                "required": true/false,
-                "name": "Answers",   
-                "placeholder": "Chọn đáp án ", 
-                "options": [{
-                    "label": contentQuestion.answerList[0],   
-                    "value": 0  
-                  }, {
-                    "label": contentQuestion.answerList[1],   
-                    "value": 1  
-                  },{
-                    "label": contentQuestion.answerList[2],   
-                    "value": 2 
-                  },{
-                    "label": contentQuestion.answerList[3],   
-                    "value": 3  
-                  }
-                ]
-            }
+		let [err, contest] = await to(Contest.findOne({ where: { contestId: Number(contestId) } }));
 
-          ]
-        }
-      };
+		var Question = app.models.Question;
+		let contentQuestion = await Question.findOne({ where: { questionId: contest.question[Number(indexQuestion)] } });
 
-    }
+		return {
+			"metadata": {
+				"app_name": "Contest",
+				"app_id": 123456,
+				"title": "BIT",
+				"submit_button": {
+					"label": "Next questions",
+					"background_color": "#6666ff",
+					"cta": "request",
+					"url": "http://bitzero.herokuapp.com/api/Contests/" + contestId + "/number/" + numberQuestion + "/right/" + contentQuestion.trueAnswer + "/score/" + score + "/nextQuestion/" + (Number(indexQuestion) + 1).toString()
+				},
+				"elements": [
+					{
+						"type": "text",
+						"style": "heading",
+						"content": contentQuestion.content
+					},
+					{
+						"type": "SCORE",
+						"style": "heading",
+						"content": score
+					},
+					{
+						"label": "Câu trả lời",
+						"type": "radio/checkbox",
+						"display_type": "inline/dialog",
+						"required": true / false,
+						"name": "Answers",
+						"placeholder": "Chọn đáp án ",
+						"options": [{
+							"label": contentQuestion.answerList[0],
+							"value": 0
+						}, {
+							"label": contentQuestion.answerList[1],
+							"value": 1
+						}, {
+							"label": contentQuestion.answerList[2],
+							"value": 2
+						}, {
+							"label": contentQuestion.answerList[3],
+							"value": 3
+						}
+						]
+					}
+
+				]
+			}
+		};
+
+	}
 
 
-    Contest.getFirstQuestion = async (req,contestId) =>{
-        let [err, contest] = await to(Contest.findOne({where: {contestId: Number(contestId)}}));
-        let numberQuestion = contest.question.length;
-        var Question = app.models.Question;
-        let contentQuestion  = await Question.findOne({where:{questionId:contest.question[0]}});
+	Contest.getFirstQuestion = async (req, contestId) => {
+		let [err, contest] = await to(Contest.findOne({ where: { contestId: Number(contestId) } }));
+		let numberQuestion = contest.question.length;
+		var Question = app.models.Question;
+		let contentQuestion = await Question.findOne({ where: { questionId: contest.question[0] } });
 
-        return {
-            "metadata": {
-              "app_name": "Contest",
-              "app_id": 123456,
-              "title": "BIT",
-              "submit_button": {
-                "label": "Next questions",
-                "background_color": "#6666ff", 
-                "cta": "request",
-                "url": "http://bitzero.herokuapp.com/api/Contests/"+contestId+"/number/"+numberQuestion.toString()+"/right/"+contentQuestion.trueAnswer+"/score/0/nextQuestion/1"
-              },
-              "elements": [
-                {
-                    "type": "text",
-                    "style": "heading",
-                    "content": contentQuestion.content
-                },
-                {
-                  "type": "SCORE",
-                  "style": "heading",
-                  "content": "0"
-                },
-                {
-                    "label": "Câu trả lời",
-                    "type": "radio/checkbox",
-                    "display_type": "inline/dialog", 
-                    "required": true/false,
-                    "name": "Answers",   
-                    "placeholder": "Chọn đáp án ", 
-                    "options": [{
-                        "label": contentQuestion.answerList[0],   
-                        "value": 0  
-                      }, {
-                        "label": contentQuestion.answerList[1],   
-                        "value": 1  
-                      },{
-                        "label": contentQuestion.answerList[2],   
-                        "value": 2 
-                      },{
-                        "label": contentQuestion.answerList[3],   
-                        "value": 3  
-                      }
-                    ]
-                }
+		return {
+			"metadata": {
+				"app_name": "Contest",
+				"app_id": 123456,
+				"title": "BIT",
+				"submit_button": {
+					"label": "Next questions",
+					"background_color": "#6666ff",
+					"cta": "request",
+					"url": "http://bitzero.herokuapp.com/api/Contests/" + contestId + "/number/" + numberQuestion.toString() + "/right/" + contentQuestion.trueAnswer + "/score/0/nextQuestion/1"
+				},
+				"elements": [
+					{
+						"type": "text",
+						"style": "heading",
+						"content": contentQuestion.content
+					},
+					{
+						"type": "SCORE",
+						"style": "heading",
+						"content": "0"
+					},
+					{
+						"label": "Câu trả lời",
+						"type": "radio/checkbox",
+						"display_type": "inline/dialog",
+						"required": true / false,
+						"name": "Answers",
+						"placeholder": "Chọn đáp án ",
+						"options": [{
+							"label": contentQuestion.answerList[0],
+							"value": 0
+						}, {
+							"label": contentQuestion.answerList[1],
+							"value": 1
+						}, {
+							"label": contentQuestion.answerList[2],
+							"value": 2
+						}, {
+							"label": contentQuestion.answerList[3],
+							"value": 3
+						}
+						]
+					}
 
-              ]
-            }
-        };
-    }
+				]
+			}
+		};
+	}
 
 
 	Contest.remoteMethod(
-		'getDescription',{ 
-            http: {path: '/getDescription', verb: 'get'},
-            accepts: [
-                {arg: 'req', type: 'object', 'http': {source: 'req'}},
-            ],
-            returns: [
-                {arg: 'data', type:'object'}]
-        }
-    )
+		'getDescription', {
+			http: { path: '/getDescription', verb: 'get' },
+			accepts: [
+				{ arg: 'req', type: 'object', 'http': { source: 'req' } },
+			],
+			returns: [
+				{ arg: 'data', type: 'object' }]
+		}
+	)
 
-  Contest.remoteMethod(
-  'getFirstQuestion',{ 
-          http: {path: '/:contestId/firstQuestion', verb: 'get'},
-          accepts: [
-              {arg: 'req', type: 'object', 'http': {source: 'req'}},
-              {arg:'contestId',type :'string',require:true}
-          ],
-          returns: [
-              {arg: 'data', type:'object'}]
-      }
-  )
-
-  Contest.remoteMethod(
-    'getNextQuestion',{ 
-            http: {path: '/:contestId/number/:numberQuestion/right/:trueAnswer/score/:score/nextQuestion/:indexQuestion', verb: 'get'},
-            accepts: [
-                {arg: 'req', type: 'object', 'http': {source: 'req'}},
-                {arg:'contestId',type :'string',require:true},
-                {arg:'numberQuestion',type :'string',require:true},
-                {arg:'trueAnswer',type :'string',require:true},
-                {arg:'score',type :'string',require:true},
-                {arg:'indexQuestion',type :'string',require:true}
-            ],
-            returns: [
-                {arg: 'data', type:'object'}]
-        }
-    )
-    
 	Contest.remoteMethod(
-		'createContest',{ 
-            http: {path: '/createContest', verb: 'post'},
-            accepts: [
-                {arg: 'req', type: 'object', 'http': {source: 'req'}},
-                {arg: 'contestName', type: 'string' },
-                {arg: 'contestDes', type: 'string'},
-                {arg: 'question', type: 'object'},
-                {arg: 'score', type: 'object'},
-                {arg: 'timeStart', type: 'date'},
-                {arg: 'timeOut', type: 'date'}
-            ],
-            returns: [{type:'object'}]
-        }
-    )
+		'getFirstQuestion', {
+			http: { path: '/:contestId/firstQuestion', verb: 'get' },
+			accepts: [
+				{ arg: 'req', type: 'object', 'http': { source: 'req' } },
+				{ arg: 'contestId', type: 'string', require: true }
+			],
+			returns: [
+				{ arg: 'data', type: 'object' }]
+		}
+	)
+
+	Contest.remoteMethod(
+		'getNextQuestion', {
+			http: { path: '/:contestId/number/:numberQuestion/right/:trueAnswer/score/:score/nextQuestion/:indexQuestion', verb: 'get' },
+			accepts: [
+				{ arg: 'req', type: 'object', 'http': { source: 'req' } },
+				{ arg: 'contestId', type: 'string', require: true },
+				{ arg: 'numberQuestion', type: 'string', require: true },
+				{ arg: 'trueAnswer', type: 'string', require: true },
+				{ arg: 'score', type: 'string', require: true },
+				{ arg: 'indexQuestion', type: 'string', require: true }
+			],
+			returns: [
+				{ arg: 'data', type: 'object' }]
+		}
+	)
+
+	Contest.remoteMethod(
+		'createContest', {
+			http: { path: '/createContest', verb: 'post' },
+			accepts: [
+				{ arg: 'req', type: 'object', 'http': { source: 'req' } },
+				{ arg: 'contestName', type: 'string' },
+				{ arg: 'contestDes', type: 'string' },
+				{ arg: 'question', type: 'object' },
+				{ arg: 'score', type: 'object' },
+				{ arg: 'timeStart', type: 'date' },
+				{ arg: 'timeOut', type: 'date' }
+			],
+			returns: [{ type: 'object' }]
+		}
+	)
 };
